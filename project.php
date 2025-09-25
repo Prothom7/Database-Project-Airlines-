a11694b5ca36dc58b8149c85b92be7fd224e433e
Prothom <prothomghosh41@gmail.com>
2025-09-26 02:31:25 +0600

    HomePage Created(Only Frontend)

---
+++ project.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>SkyHigh Airlines</title>
+    <link rel="stylesheet" href="home.css">
+</head>
+<body>
+
+    <?php include 'home.html'; ?>
+
+</body>
+</html>
